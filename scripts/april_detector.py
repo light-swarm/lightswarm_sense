ad1f4a82180e7903760b710e6553c6967e96abee
Palash Nandy <palash@media.mit.edu>
2014-04-09 19:43:41 -0400

    added config file

--- scripts/april_detector.py
+++ scripts/april_detector.py
@@ -6,40 +6,46 @@ from lightswarm_core.msg import Cylinder
 from april_tag.msg import AprilTagList
 from april_tag.msg import AprilTag
 
+
 import numpy as np
 import affine
+import yaml
 
 WORLD_LIMIT = 100
 GHETTO_CALIBRATION_X_OFFSET = 0
 GHETTO_CALIBRATION_Z_OFFSET = 202
 
-
-camera_points = np.float32(np.array([[0,  20, 180], [100,   20, 180], [0, 20, 280], [100, 20, 280]]))
-world_points = np.float32(np.array([[0, -100,  20], [100, -100,  20], [0,  0,  20], [100,  0,  20]]))
+CONFIG_FILE = 'lightswarm_core/params/config.yaml'
 
 class AprilDetector(object):
 	def __init__(self):
 		rospy.init_node('april_detector')
 		self.sub = rospy.Subscriber('/april_tags', AprilTagList, self.april_callback)
 		self.pub = rospy.Publisher('/objects', Objects)
+
+		config_filename = rospy.get_param('config_file', CONFIG_FILE)
+		self.config_map = yaml.load(open(config_filename))
 		self.setup_camera_transform()
 
 	def setup_camera_transform(self):
 		#retval, matrix, inliners = cv2.estimateAffine3D(camera_points, world_points)
 		#assert retval == 0, 'camera transform failed'
-
+		camera_points = np.asarray(self.config_map.get('sense_camera_points'))
+		world_points = np.asarray(self.config_map.get('sense_world_points'))
 		self.transform_matrix = affine.affine_matrix_from_points(camera_points.T,
 																 world_points.T,
 																 shear=False,
 																 scale=False)
+		print 'got camera points %s' % str(camera_points)
+		print 'got world points %s' % str(world_points)
 
 	def transform_camera_coordinates(self, cx, cy, cz):
 		wx = cx - GHETTO_CALIBRATION_X_OFFSET
 		wy = cz - GHETTO_CALIBRATION_Z_OFFSET
 		wz = 0
-		#wx, wy, wz, junk = self.transform_matrix.dot([cx, cy, cz, 1])
-		#int_cord = ['%s' % int(x) for x in (cx, cy, cz, wx, wy, wz)]
-		#print '%s -> %s' % (':'.join(int_cord[:3]), ':'.join(int_cord[3:]))
+		wx, wy, wz, junk = self.transform_matrix.dot([cx, cy, cz, 1])
+		int_cord = ['%s' % int(x) for x in (cx, cy, cz, wx, wy, wz)]
+		print '%s -> %s' % (':'.join(int_cord[:3]), ':'.join(int_cord[3:]))
 		return wx, wy, wz
 
 
